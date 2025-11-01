2f634108b74f117c79b2594b0a147315ab54c996
MN15LONER <mfune@outlook.com>
2025-11-01 20:00:42 +0200

    restarting

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Mfuneko_Ndeya_Portfolio/',
-  plugins: [react()],
-})
